126b47e500182834acc4e5acd4f53b50b4263d3f
tn-als <tnals@kyonggi.ac.kr>
2024-07-22 15:37:00 +0900

    feat/current-location

--- app/src/main/java/com/kseb/smart_car/presentation/main/LocationActivity.kt
+++ app/src/main/java/com/kseb/smart_car/presentation/main/LocationActivity.kt
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.kakao.sdk.common.KakaoSdk.appKey
@@ -19,8 +20,9 @@ import com.kakaomobility.knsdk.common.objects.KNError_Code_C302
 import com.kakaomobility.knsdk.ui.view.KNNaviView
 import com.kseb.smart_car.BuildConfig
 import com.kseb.smart_car.databinding.ActivityLocationBinding
-import com.kseb.smart_car.presentation.join.JoinActivity
+import kotlinx.coroutines.launch
 import kotlin.math.roundToInt
+import kotlin.properties.Delegates
 
 class LocationActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLocationBinding
@@ -28,6 +30,9 @@ class LocationActivity : AppCompatActivity() {
     private lateinit var knNaviView: KNNaviView
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private val REQUEST_CODE_LOCATION_PERMISSION = 123
+
+    private var longitude by Delegates.notNull<Double>()
+    private var latitude by Delegates.notNull<Double>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,7 +43,6 @@ class LocationActivity : AppCompatActivity() {
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
         initializeKNSDK()
-        checkLocation()
     }
 
     private fun initializeKNSDK() {
@@ -57,9 +61,7 @@ class LocationActivity : AppCompatActivity() {
                     }
                 } else {
                     // 인증 완료
-                    val intent = Intent(this, MainActivity::class.java)
-                    startActivity(intent)
-                    finish()
+                    checkLocation()
                 }
             })
     }
@@ -68,7 +70,6 @@ class LocationActivity : AppCompatActivity() {
         // 위치 권한이 이미 허용되어 있는지 확인
         if (isLocationPermissionGranted()) {
             // 위치 기능 사용 가능
-            //initLocationManager()
             if (ActivityCompat.checkSelfPermission(
                     this,
                     Manifest.permission.ACCESS_FINE_LOCATION
@@ -77,40 +78,56 @@ class LocationActivity : AppCompatActivity() {
                     Manifest.permission.ACCESS_COARSE_LOCATION
                 ) != PackageManager.PERMISSION_GRANTED
             ) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
+                // 권한이 없는 경우 처리
+                requestLocationPermission()
                 return
             }
+
+            // 위치 요청
             fusedLocationClient.lastLocation.addOnSuccessListener { location ->
                 if (location != null) {
-                    // 위치 정보를 KNNaviView에 전달
+                    // 위치 정보를 가져온 후 처리
+                    longitude = location.longitude
+                    latitude = location.latitude
+                    Log.d("locationactivity", "long:${longitude}, lati:${latitude}")
+
+                    // KNNaviView에 위치 정보 전달
                     knNaviView.mapComponent.mapView.getMapToCenter().let { pos ->
                         val point = KATECToWGS84(
                             pos.x.roundToInt().toDouble(),
                             pos.y.roundToInt().toDouble()
                         )
-                        val alerts = AlertDialog.Builder(this)
+                        /*// 위치 정보를 보여주는 다이얼로그
+                        AlertDialog.Builder(this)
                             .setTitle("현재 위치")
                             .setMessage("longitude: ${point.x}\nlatitude: ${point.y}")
-                        alerts.setCancelable(true)
-                        alerts.setNegativeButton("close")
-                        { dialog, _ ->
-                            dialog.dismiss()
-                        }
-                        //alerts.create().show()
+                            .setCancelable(true)
+                            .setNegativeButton("close") { dialog, _ -> dialog.dismiss() }
+                            .create()
+                            .show()*/
+
+                        // 위치 정보를 사용하여 다음 Activity로 이동
+                        val intent = Intent(this, MainActivity::class.java)
+                        intent.putExtra("longitude", longitude)
+                        intent.putExtra("latitude", latitude)
+                        startActivity(intent)
+                        finish()
                     }
+                } else {
+                    Log.e("locationactivity", "Location is null")
                 }
             }
-
         } else {
             // 위치 권한 요청
             requestLocationPermission()
         }
+    }
+
+    private fun isLocationPermissionGranted(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
     }
 
     private fun requestLocationPermission() {
@@ -122,10 +139,5 @@ class LocationActivity : AppCompatActivity() {
         checkLocation()
     }
 
-    private fun isLocationPermissionGranted(): Boolean {
-        return ContextCompat.checkSelfPermission(
-            this,
-            Manifest.permission.ACCESS_FINE_LOCATION
-        ) == PackageManager.PERMISSION_GRANTED
-    }
+
 }
