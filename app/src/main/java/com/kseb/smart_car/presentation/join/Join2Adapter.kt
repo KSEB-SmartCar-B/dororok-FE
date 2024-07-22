2dbbe03c0ac9940648980c57cc36835c74946343
zwongraphic <bababaenjooo@gmail.com>
2024-07-22 13:47:55 +0900

    refactor/join

--- app/src/main/java/com/kseb/smart_car/presentation/join/Join2Adapter.kt
+++ app/src/main/java/com/kseb/smart_car/presentation/join/Join2Adapter.kt
@@ -3,12 +3,36 @@ package com.kseb.smart_car.presentation.join
 import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.RecyclerView
 import com.kseb.smart_car.R
 import com.kseb.smart_car.databinding.ItemGenreBinding
 
-class Join2Adapter(): RecyclerView.Adapter<Join2ViewHolder>() {
-    private val genreList = mutableListOf<Join2>()
+class Join2Adapter(private val onButtonClick: (String) -> Unit): RecyclerView.Adapter<Join2Adapter.Join2ViewHolder>() {
+
+    private val genreList = mutableListOf<String>()
+
+    inner class Join2ViewHolder(
+        private val binding: ItemGenreBinding
+
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun onBind(genre: String, onButtonClick: (String) -> Unit) {
+            binding.btnGenre.text = genre
+
+            genreButton(onButtonClick)
+        }
+
+        private fun genreButton(onButtonClick: (String) -> Unit) {
+            val btnGenre = binding.btnGenre
+
+            btnGenre.isSelected = false
+
+            btnGenre.setOnClickListener {
+                btnGenre.isSelected = !btnGenre.isSelected
+                onButtonClick(btnGenre.text.toString())
+            }
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Join2ViewHolder {
         val binding = ItemGenreBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -19,11 +43,10 @@ class Join2Adapter(): RecyclerView.Adapter<Join2ViewHolder>() {
 
     override fun onBindViewHolder(holder: Join2ViewHolder, position: Int) {
         val item = genreList[position]
-        holder.onBind(item)
+        holder.onBind(item, onButtonClick)
     }
 
-    @SuppressLint("NotifyDataSetChanged")
-    fun getList(list: List<Join2>) {
+    fun getList(list: List<String>) {
         genreList.addAll(list)
         notifyDataSetChanged()
     }
--- app/src/main/java/com/kseb/smart_car/presentation/join/Join2ViewModel.kt
+++ app/src/main/java/com/kseb/smart_car/presentation/join/Join2ViewModel.kt
@@ -3,23 +3,27 @@ package com.kseb.smart_car.presentation.join
 import androidx.lifecycle.ViewModel
 
 class Join2ViewModel: ViewModel() {
-    fun makeList(): List<Join2> {
+    fun makeList(): List<String> {
         return listOf(
-            Join2("댄스"),
-            Join2("국내 힙합"),
-            Join2("팝"),
-            Join2("국내 R&B"),
-            Join2("JPOP"),
-            Join2("클래식"),
-            Join2("일렉트로닉"),
-            Join2("펑크"),
-            Join2("발라드"),
-            Join2("트로트"),
-            Join2("밴드"),
-            Join2("어쿠스틱"),
-            Join2("인디"),
-            Join2("재즈"),
-            Join2("컨트리"),
-        )
+            "댄스",
+            "발라드",
+            "인디",
+            "트로트",
+            "OST",
+            "POP",
+            "J-POP",
+            "재즈",
+            "클래식",
+            "뉴에이지",
+            "월드뮤직",
+            "일렉트로니카",
+            "국내 록/메탈",
+            "해외 록/메탈",
+            "국내 랩/힙합",
+            "해외 랩/힙합",
+            "국내 R&B/Soul",
+            "해외 R&B/Soul",
+            "국내 포크/블루스",
+            "해외 포크/블루스/컨트리")
     }
 }
